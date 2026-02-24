8842be4c347b28e46745889481203524843c3537
Tina-l7 <letina@umich.edu>
2026-02-24 15:42:44 -0500

    finished task 3

--- discussion7.py
+++ discussion7.py
@@ -106,8 +106,15 @@ def write_summary_csv(out_filename, avg_prices):
         None
             Writes a CSV file with header: neighbourhood_group, room_type, average_price
     """
-    pass
+    with open(out_filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['neighbourhood_group', 'room_type', 'average_price'])
+        
+        for key, val in avg_prices.items():
+            row = [key[0], key[1], val]
+            writer.writerow(row)
 
+#finished task
 ###############################################################################
 ##### UNIT TESTS (Do not modify the code below!)
 ###############################################################################
